c8f2f441e11f2d5c949a480677ace5993da6c72d
trujillonefi12 <trujillonefi12@gmail.com>
2024-12-12 16:15:04 -0700

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Slot, Stack } from "expo-router";
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      ></Stack.Screen>
+    </Stack>
+  );
+};
+
+export default RootLayout;
